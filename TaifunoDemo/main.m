055188fbaafe3b6ebbac8e1840764894c758781e
Artem Olkov <art.olkov@gmail.com>
2014-10-11 18:42:40 +0400

    Initial Commit

---
+++ TaifunoDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TaifunoDemo
+//
+//  Created by Artem Olkov on 11/10/14.
+//  Copyright (c) 2014 taifuno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
